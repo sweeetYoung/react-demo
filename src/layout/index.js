cf9b8d753ab01fbb0375be27f12843920c75de74
coolyy <kklubeak@163.com>
2021-09-08 10:45:02 +0800

    hook

--- src/layout/index.js
+++ src/layout/index.js
@@ -10,53 +10,92 @@ import {
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
-class BasicLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-  componentDidMount() {
-  }
-  render () {
-    return (
-      <Layout>
-        <Sider>
-          <div className="logo">Model Insights</div>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Models
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Modules
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="Tools">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            <Breadcrumb style={{ margin: '16px' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-          </Header>
-          <Content style={{ margin: '16px' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              Bill is a cat.
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
-        {/*<Route exact path="/home" component={home} />*/}
-        {/*<Route path="/workBench" component={workBench} />*/}
-        {/*<Route path="/login" component={login} />*/}
-      </Layout>
-    )
-  }
+function BasicLayout () {
+  return (
+  <Layout>
+    <Sider>
+      <div className="logo">Model Insights</div>
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        <Menu.Item key="1" icon={<PieChartOutlined />}>
+          Models
+        </Menu.Item>
+        <Menu.Item key="2" icon={<DesktopOutlined />}>
+          Modules
+        </Menu.Item>
+        <SubMenu key="sub1" icon={<UserOutlined />} title="Tools">
+          <Menu.Item key="3">Tom</Menu.Item>
+          <Menu.Item key="4">Bill</Menu.Item>
+          <Menu.Item key="5">Alex</Menu.Item>
+        </SubMenu>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout">
+      <Header className="site-layout-background" style={{ padding: 0 }}>
+        <Breadcrumb style={{ margin: '16px' }}>
+          <Breadcrumb.Item>User</Breadcrumb.Item>
+          <Breadcrumb.Item>Bill</Breadcrumb.Item>
+        </Breadcrumb>
+      </Header>
+      <Content style={{ margin: '16px' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          Bill is a cat.
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+    {/*<Route exact path="/home" component={home} />*/}
+    {/*<Route path="/workBench" component={workBench} />*/}
+    {/*<Route path="/login" component={login} />*/}
+  </Layout>
+  )
 }
+// class BasicLayout extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     }
+//   }
+//   componentDidMount() {
+//   }
+//   render () {
+//     return (
+//       <Layout>
+//         <Sider>
+//           <div className="logo">Model Insights</div>
+//           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+//             <Menu.Item key="1" icon={<PieChartOutlined />}>
+//               Models
+//             </Menu.Item>
+//             <Menu.Item key="2" icon={<DesktopOutlined />}>
+//               Modules
+//             </Menu.Item>
+//             <SubMenu key="sub1" icon={<UserOutlined />} title="Tools">
+//               <Menu.Item key="3">Tom</Menu.Item>
+//               <Menu.Item key="4">Bill</Menu.Item>
+//               <Menu.Item key="5">Alex</Menu.Item>
+//             </SubMenu>
+//           </Menu>
+//         </Sider>
+//         <Layout className="site-layout">
+//           <Header className="site-layout-background" style={{ padding: 0 }}>
+//             <Breadcrumb style={{ margin: '16px' }}>
+//               <Breadcrumb.Item>User</Breadcrumb.Item>
+//               <Breadcrumb.Item>Bill</Breadcrumb.Item>
+//             </Breadcrumb>
+//           </Header>
+//           <Content style={{ margin: '16px' }}>
+//             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+//               Bill is a cat.
+//             </div>
+//           </Content>
+//           <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+//         </Layout>
+//         {/*<Route exact path="/home" component={home} />*/}
+//         {/*<Route path="/workBench" component={workBench} />*/}
+//         {/*<Route path="/login" component={login} />*/}
+//       </Layout>
+//     )
+//   }
+// }
 export default BasicLayout
 
